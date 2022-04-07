586358b5ad4746058bbb0f1565f9798c54fea344
David Walter <hello@davidwalter.at>
2022-04-07 11:26:26 +0200

    Small restructuring

--- Sources/CustomAlert/Helpers.swift
+++ Sources/CustomAlert/Helpers.swift
@@ -55,3 +55,30 @@ extension View {
         )
     }
 }
+
+// MARK: - Blur View
+
+// Using UIViewRepresentable BlurView for backwards compatibility
+struct BlurView: UIViewRepresentable {
+    let style: UIBlurEffect.Style
+
+    func makeUIView(context: UIViewRepresentableContext<BlurView>) -> UIView {
+        let view = UIView(frame: .zero)
+        view.backgroundColor = .clear
+        let blurEffect = UIBlurEffect(style: style)
+        let blurView = UIVisualEffectView(effect: blurEffect)
+        blurView.translatesAutoresizingMaskIntoConstraints = false
+        view.insertSubview(blurView, at: 0)
+        NSLayoutConstraint.activate([
+            blurView.heightAnchor.constraint(equalTo: view.heightAnchor),
+            blurView.widthAnchor.constraint(equalTo: view.widthAnchor)
+        ])
+        return view
+    }
+
+    func updateUIView(_ uiView: UIView,
+                      context: UIViewRepresentableContext<BlurView>) {
+
+    }
+}
+

ae4e0d7bc69e0acad96e869db751bc6dd146b760
David Walter <hello@davidwalter.at>
2022-07-09 12:04:12 +0200

    Add callback version for title

--- Sources/CustomAlert/API.swift
+++ Sources/CustomAlert/API.swift
@@ -97,4 +97,54 @@ public extension View {
     where Title: StringProtocol, Content: View, Actions: View {
         self.customAlert(Text(title), isPresented: isPresented, content: content, actions: actions)
     }
+    
+    /// Presents an alert when a given condition is true, using an optional text view for
+    /// the title.
+    ///
+    /// All actions in an alert dismiss the alert after the action runs.
+    ///
+    /// - Parameters:
+    ///   - isPresented: A binding to a Boolean value that determines whether to
+    ///     present the alert. When the user presses or taps one of the alert's
+    ///     actions, the system sets this value to `false` and dismisses.
+    ///   - title: Callback for the optional title of the alert.
+    ///   - content: A ``ViewBuilder`` returing the alerts main view.
+    ///   - actions: A ``ViewBuilder`` returning the alert's actions.
+    @ViewBuilder func customAlert<Content, Actions>(isPresented: Binding<Bool>,
+                                                    title: @escaping () -> Text?,
+                                                    @ViewBuilder content: @escaping () -> Content,
+                                                    @ViewBuilder actions: @escaping () -> Actions) -> some View
+    where Content: View, Actions: View {
+        if #available(iOS 14, *) {
+            onChange(of: isPresented.wrappedValue) { value in
+                if value {
+                    AlertWindow.present(CustomAlert(title: title(), isPresented: isPresented, content: content, actions: actions))
+                } else {
+                    AlertWindow.dismiss()
+                }
+            }
+            .disabled(isPresented.wrappedValue)
+            .onAppear {
+                guard isPresented.wrappedValue else { return }
+                AlertWindow.present(CustomAlert(title: title(), isPresented: isPresented, content: content, actions: actions))
+            }
+            .onDisappear {
+                AlertWindow.dismiss()
+            }
+        } else {
+            onReceive(Just(isPresented.wrappedValue)) { value in
+                if value {
+                    AlertWindow.present(CustomAlert(title: title(), isPresented: isPresented, content: content, actions: actions))
+                } else {
+                    // Cannot use this to hide the alert on iOS 13 because `onReceive`
+                    // will get called for all alerts if there are multiple on a single view
+                    // causing all alerts to be hidden immediately after appearing
+                }
+            }
+            .disabled(isPresented.wrappedValue)
+            .onDisappear {
+                AlertWindow.dismiss()
+            }
+        }
+    }
 }

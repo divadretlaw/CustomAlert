868d20ab506b5bd147e76f5d7fc5be3260bdb029
David Walter <hello@davidwalter.at>
2022-08-19 18:49:09 +0200

    Add basic demo

---
+++ Demo/Demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by David Walter on 19.08.22.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

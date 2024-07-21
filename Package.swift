ddf88002e5f84c27f053d0bcc3ad95872c2a22e1
David Walter <hello@davidwalter.at>
2024-07-21 10:00:01 -0300

    Update WindowKit

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/divadretlaw/WindowKit", from: "2.2.0")
+        .package(url: "https://github.com/divadretlaw/WindowKit", from: "2.3.0")
     ],
     targets: [
         .target(name: "CustomAlert", dependencies: ["WindowKit"])

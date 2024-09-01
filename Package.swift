b04486b90fb1474d891e6782b058c0354d7e12c6
David Walter <hello@davidwalter.at>
2024-09-01 11:46:02 +0200

    Update WindowKit

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/divadretlaw/WindowKit", from: "2.4.0")
+        .package(url: "https://github.com/divadretlaw/WindowKit", from: "2.5.0")
     ],
     targets: [
         .target(name: "CustomAlert", dependencies: ["WindowKit"])

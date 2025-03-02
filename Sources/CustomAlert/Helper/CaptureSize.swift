b548825491a1b058bee78e235d1092a0f8962a5e
David Walter <hello@davidwalter.at>
2025-03-02 14:02:18 +0100

    Streamline CaptureSize

--- Sources/CustomAlert/Helper/CaptureSize.swift
+++ Sources/CustomAlert/Helper/CaptureSize.swift
@@ -25,45 +25,77 @@ private struct IntrinsicSafeAreaPreferenceKey: PreferenceKey {
 
 extension View {
     func captureSize(_ size: Binding<CGSize>) -> some View {
-        background(
-            GeometryReader { proxy in
-                Color.clear
-                    .preference(key: IntrinsicContentSizePreferenceKey.self, value: proxy.size)
-                    .onPreferenceChange(IntrinsicContentSizePreferenceKey.self) {
-                        size.wrappedValue = $0
-                    }
-            }
-        )
+        captureSize { anchor, proxy in
+            anchor
+                .preference(key: IntrinsicContentSizePreferenceKey.self, value: proxy.size)
+                .onPreferenceChange(IntrinsicContentSizePreferenceKey.self) {
+                    size.wrappedValue = $0
+                }
+        }
     }
     
     func captureTotalSize(_ size: Binding<CGSize>) -> some View {
-        background(
-            GeometryReader { proxy in
-                Color.clear
-                    .preference(key: IntrinsicContentSizePreferenceKey.self, value: proxy.size)
-                    .onPreferenceChange(IntrinsicContentSizePreferenceKey.self) {
-                        size.wrappedValue = $0 + proxy.safeAreaInsets
-                    }
-                    .preference(key: IntrinsicSafeAreaPreferenceKey.self, value: proxy.safeAreaInsets)
-                    .onPreferenceChange(IntrinsicSafeAreaPreferenceKey.self) {
-                        size.wrappedValue = proxy.size + $0
-                    }
-            }
-        )
+        captureSize { anchor, proxy in
+            anchor
+                .preference(key: IntrinsicContentSizePreferenceKey.self, value: proxy.size)
+                .onPreferenceChange(IntrinsicContentSizePreferenceKey.self) {
+                    size.wrappedValue = $0 + proxy.safeAreaInsets
+                }
+                .preference(key: IntrinsicSafeAreaPreferenceKey.self, value: proxy.safeAreaInsets)
+                .onPreferenceChange(IntrinsicSafeAreaPreferenceKey.self) {
+                    size.wrappedValue = proxy.size + $0
+                }
+        }
     }
     
     func captureSafeAreaInsets(_ safeArea: Binding<EdgeInsets>) -> some View {
-        background(
-            GeometryReader { proxy in
-                Color.clear
-                    .preference(key: IntrinsicSafeAreaPreferenceKey.self, value: proxy.safeAreaInsets)
-                    .onPreferenceChange(IntrinsicSafeAreaPreferenceKey.self) {
-                        safeArea.wrappedValue = $0
-                    }
-            }
-        )
+        captureSize { anchor, proxy in
+            anchor
+                .preference(key: IntrinsicSafeAreaPreferenceKey.self, value: proxy.safeAreaInsets)
+                .onPreferenceChange(IntrinsicSafeAreaPreferenceKey.self) {
+                    safeArea.wrappedValue = $0
+                }
+        }
     }
 }
+
+private extension View {
+    func captureSize<Result>(
+        @ViewBuilder builder: @escaping (_ anchor: AnyView, _ proxy: SendableGeometryProxy) -> Result
+    ) -> some View where Result: View {
+        modifier(CaptureSize(builder: builder))
+    }
+}
+
+private struct CaptureSize<Result>: ViewModifier where Result: View {
+    let builder: (_ anchor: AnyView, _ proxy: SendableGeometryProxy) -> Result
+    
+    init(
+        @ViewBuilder builder: @escaping (_ anchor: AnyView, _ proxy: SendableGeometryProxy) -> Result
+    ) {
+        self.builder = builder
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .background {
+                GeometryReader { proxy in
+                    builder(AnyView(Color.clear), SendableGeometryProxy(proxy: proxy))
+                }
+            }
+    }
+}
+
+private struct SendableGeometryProxy: Sendable {
+    let size: CGSize
+    let safeAreaInsets: EdgeInsets
+    
+    init(proxy: GeometryProxy) {
+        self.size = proxy.size
+        self.safeAreaInsets = proxy.safeAreaInsets
+    }
+}
+
 
 private extension CGSize {
     static func + (lhs: CGSize, rhs: EdgeInsets) -> CGSize {

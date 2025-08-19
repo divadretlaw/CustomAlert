ebd2b1595b41944425d966dcaee50c63386399d2
David Walter <hello@davidwalter.at>
2025-08-19 19:24:15 +0200

    Improve CustomAlertRow

--- Sources/CustomAlert/Views/CustomAlertRow.swift
+++ Sources/CustomAlert/Views/CustomAlertRow.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 /// Display a custom alert inlined into a `List`
 public struct CustomAlertRow<Content, Actions>: View where Content: View, Actions: View {
+    @Environment(\.customAlertConfiguration) private var configuration
     @Binding var isPresented: Bool
     
     let content: Content
@@ -18,17 +19,17 @@ public struct CustomAlertRow<Content, Actions>: View where Content: View, Action
         if isPresented {
             VStack(spacing: 0) {
                 content
-                
-                if #available(iOS 18.0, *) {
-                    ForEach(subviews: actions) { child in
-                        Divider()
-                        child
-                    }
-                } else {
-                    _VariadicView.Tree(ContentLayout()) {
-                        actions
-                    }
+
+                switch configuration.alert.dividerVisibility {
+                case .hidden, .automatic:
+                    EmptyView()
+                case .visible:
+                    Divider()
                 }
+                _VariadicView.Tree(ContentLayout()) {
+                    actions
+                }
+                .padding(configuration.alert.actionPadding)
             }
             .buttonStyle(.alert(triggerDismiss: false))
             .listRowInsets(.zero)
@@ -53,13 +54,16 @@ public struct CustomAlertRow<Content, Actions>: View where Content: View, Action
         self.content = content()
         self.actions = actions()
     }
-    
-    @available(iOS, introduced: 14.0, deprecated: 18.0, message: "Use `ForEach(subviewOf:content:)` instead")
-    struct ContentLayout: _VariadicView_ViewRoot {
+
+    @MainActor struct ContentLayout: _VariadicView_ViewRoot {
+        @Environment(\.customAlertConfiguration) private var configuration
+
         func body(children: _VariadicView.Children) -> some View {
-            VStack(spacing: 0) {
-                ForEach(children) { child in
-                    Divider()
+            VStack(spacing: configuration.button.spacing) {
+                ForEach(Array(children.enumerated()), id: \.offset) { index, child in
+                    if index != 0, !configuration.button.hideDivider {
+                        Divider()
+                    }
                     child
                 }
             }
@@ -83,7 +87,8 @@ struct CustomAlertRow_Preview: PreviewProvider {
                     } label: {
                         Text("Show Custom Alert Row")
                     }
-                    
+                }
+                Section {
                     CustomAlertRow(isPresented: $isPresented) {
                         Text("Hello World")
                             .padding()

b6a7c728d58f497f32ae3156dac92893f7b317b0
David Walter <hello@davidwalter.at>
2025-11-08 11:10:04 +0100

    Add missing convenience API

--- Sources/CustomAlert/API+Bool.swift
+++ Sources/CustomAlert/API+Bool.swift
@@ -307,4 +307,26 @@ public extension View {
     ) -> some View where Content: View {
         customAlert(title(), isPresented: isPresented, content: content, actions: { /* no actions */ })
     }
+
+    /// Presents an alert when a given condition is true, using an optional text view for
+    /// the title.
+    ///
+    /// All actions in an alert dismiss the alert after the action runs.
+    ///
+    /// - Parameters:
+    ///   - isPresented: A binding to a Boolean value that determines whether to
+    ///     present the alert. When the user presses or taps one of the alert's
+    ///     actions, the system sets this value to `false` and dismisses.
+    ///   - windowScene: The window scene to present the alert on.
+    ///   - title: Callback for the optional title of the alert.
+    ///   - content: A `ViewBuilder` returing the alerts main view.
+    @MainActor
+    func customAlert<Content>(
+        isPresented: Binding<Bool>,
+        on windowScene: UIWindowScene,
+        title: @escaping () -> Text?,
+        @ViewBuilder content: @escaping () -> Content
+    ) -> some View where Content: View {
+        customAlert(title(), isPresented: isPresented, on: windowScene, content: content, actions: { /* not action */})
+    }
 }
